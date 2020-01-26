646b31654780d25af114fbd80e450f4450538bdd
midnightsea <kevin.charles.van@gmail.com>
2020-01-26 08:13:50 -0700

    add package to deploy to pages

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-fractal/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/Resume/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Kevin Van',
   subHeading: 'Full Stack Developer. Loves learning new technologies  ',
   location: 'Phoenix, Arizona',
